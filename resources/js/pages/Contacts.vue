efb482e471aaab505a82970a74e15e36c396caf6
LorisP96 <lorispalazzo96@gmail.com>
2022-09-16 20:29:10 +0200

    fine

--- resources/js/pages/Contacts.vue
+++ resources/js/pages/Contacts.vue
@@ -1,31 +1,77 @@
 <template>
     <section class="container">
         <h1>Contattaci</h1>
-    <form>
-        <div class="form-group">
-            <label for="user-name">Nome</label>
-            <input type="text" class="form-control" id="user-name">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlTextarea1">Scrivici qualcosa</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
-        
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+
+        <div v-if="success" class="alert alert-success _myalert" role="alert">Grazie per averci contattato</div>
+
+        <form @submit.prevent="sendMessage()">
+            <div class="form-group">
+                <label for="user-name">Nome</label>
+                <input v-model="userName" type="text" class="form-control mb-2" id="user-name">
+                <div v-if="errors.name" class="alert alert-danger" role="alert">{{errors.name}}</div>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email</label>
+                <input v-model="userEmail" type="email" class="form-control mb-2" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div v-if="errors.email" class="alert alert-danger" role="alert">{{errors.email}}</div>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Scrivici qualcosa</label>
+                <textarea v-model="userText" class="form-control mb-2" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <div v-if="errors.text" class="alert alert-danger" role="alert">{{errors.text}}</div>
+            </div>
+            
+            <button type="submit" class="btn btn-primary" :disabled="sending">Submit</button>
+        </form>
     </section>
 </template>
 
 <script>
+
 export default {
-    name: 'Contacts'
+    name: 'Contacts',
+    data() {
+        return {
+            userName: '',
+            userEmail: '',
+            userText: '',
+            success: false,
+            errors: {},
+            sending: false,
+        }
+    },
+    methods: {
+        sendMessage() {
+            this.sending = true;
+            axios.post('/api/leads', {
+                name: this.userName,
+                email: this.userEmail,
+                text: this.userText,
+            })
+            .then((response) => {
+                if(response.data.success) {
+                    this.success = true;
+                    this.userName = '';
+                    this.userEmail = '';
+                    this.userText = '';
+                } else {
+                    this.errors = response.data.errors;
+                }
+
+                this.sending = false;
+            });
+        }
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.container {
+    ._myalert {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+}
 </style>
